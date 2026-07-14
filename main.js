1993b950b4fb36f4385d0895f090a36459e4f133
studyamericateam-commits <studyamericateam@gmail.com>
2026-07-14 20:59:27 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
+import App from "./app-vega.js";
 
 createRoot(document.getElementById("root")).render(
   React.createElement(React.StrictMode, null, React.createElement(App))
